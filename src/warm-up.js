ad9440332c9925272f4715f292e3405f9099a7e1
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-06 15:54:09 -0600

    initial Commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -9,30 +9,34 @@
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
+var food = "Tacos";
+var type = "Lunch";
+var inStock = 8
 
 
 
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+var inStock = inStock * 2
 
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+var inStock = inStock - 1
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = false
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+if (inStock < 20) {
+  shouldOrderMore = true
+} else {
+  shouldOrderMore = false
+}
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,8 +44,15 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
+var stockCount = 2
+var food = "Pizza"
+if (stockCount > 1) {
+  console.log("I'm going to eat some "  + food )
+} else {
+  console.log("We need to get some more " + food)
+}
 
 
 
