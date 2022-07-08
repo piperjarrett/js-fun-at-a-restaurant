af8f8b47d2c289175a1772f1d7a228bde70d8b58
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-08 16:27:05 -0600

    Complete

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(type) {
+return `Delicious ${type}`
 }
 
+ function createMenuItem(name, price, type) {
+return {
+   name,
+  price,
+   type
+ }
+}
 
+function addIngredients(ingredientsOne, ingredients) {
+  if (ingredients.includes(ingredientsOne)) {
+    ingredients.pop();
+  }
+
+ingredients[ingredients.length] = ingredientsOne;
+return ingredients
+}
+ //return ingredients
+
+ function formatPrice(price) {
+   return `$${price}`
+ }
+
+function decreasePrice(price) {
+  return price*.9
+}
+
+function createRecipe(title, ingredients, type) {
+ var recipe = {
+   title,
+  ingredients,
+  type
+ }
+  return recipe
+
+}
+//  ingredients, ingredientsTwo, ingredientsThree
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
