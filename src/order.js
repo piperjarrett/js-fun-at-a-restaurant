44f533516fe115524221c4fead52933f6da2a8f4
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-10 15:48:40 -0600

    Completed Orders

--- src/order.js
+++ src/order.js
@@ -1,7 +1,36 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order)
+  }
+}
 
+function refundOrder(order, deliveryOrders) {
+  for (var i = 0; i < refundOrder.length; i++) {
+    if (deliveryOrders[i].orderNumber === order) {
+      deliveryOrders.splice(i,1)
+    }
+  }
+}
+function listItems(deliveryOrders) {
+  var item = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    item.push(deliveryOrders[i].item)
+}
+  return item.join(', ')
+}
+function searchOrder(deliveryOrders, order) {
+//console.log(deliveryOrders.length)
+var inList = false
+for (var i = 0; i < deliveryOrders.length; i++) {
+if (deliveryOrders[i].item === order)
+ inList = true
+
+}
+return inList
+}
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,45 @@
+function createRestaurant(name) {
+  var pizzaRestaurant = {
+      name,
+      menus: { breakfast: [], lunch: [], dinner: [] }
+  }
+  return pizzaRestaurant
+}
+
+ function addMenuItem(pizzaRestaurant, bbqPizza) {
+
+   if (bbqPizza.type === "lunch" && !pizzaRestaurant.menus.lunch.includes(bbqPizza)) {
+     pizzaRestaurant.menus.lunch.push(bbqPizza)
+   }
+   else if (bbqPizza.type === "breakfast" && !pizzaRestaurant.menus.breakfast.includes(bbqPizza)) {
+     pizzaRestaurant.menus.breakfast.push(bbqPizza)
+   }
+   else if (bbqPizza.type === "dinner" && !pizzaRestaurant.menus.dinner.includes(bbqPizza)) {
+    pizzaRestaurant.menus.dinner.push(bbqPizza)
+  }
+}
+function removeMenuItem(pizzaRestaurant, bbqPizza, type) {
+  var menuItem = pizzaRestaurant.menus[type]
+  for (var i = 0; i < menuItem.length; i++) {
+  if ( menuItem[i].name = bbqPizza) {
+    menuItem.splice(i, 1)
+    return `No one is eating our ${bbqPizza} - it has been removed from the ${type} menu!`
+  }
+
+  }
+
+  //else if (bbqPizza.type === "breakfast" && pizzaRestaurant.menus.includes(bbqPizza)) {
+    //pizzaRestaurant.menus.splice()
+  //}
+  //else if (bbqPizza.type === "dinner" && pizzaRestaurant.menus.includes(bbqPizza)) {
+   //pizzaRestaurant.menus.splice()
+ //}
+}
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
